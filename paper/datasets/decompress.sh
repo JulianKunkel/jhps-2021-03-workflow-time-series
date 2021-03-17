908dc60fe4c175c7ec2acfec33d0dc314499f0fe
Julian M. Kunkel <juliankunkel@googlemail.com>
2021-03-17 13:42:04 +0100

    Repacking

--- paper/datasets/decompress.sh
+++ paper/datasets/decompress.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-filenames=( \
-	"job_codings.csv" \
-	"job_io_duration.csv" \
-	"job_metrics.csv" \
-	"job_phases.csv" \
-	"job_metadata.csv" \
-)
-
-for filename in ${filenames[@]}; do
-	echo "Decompressing ${filename}.tar.xz"
-	tar -xJf "${filename}.tar.xz" 
+for filename in *.xz; do
+	echo "Decompressing ${filename}"
+	tar -xJf "${filename}" 
 done
